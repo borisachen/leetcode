ef7e6d08eac0cc4294fd001e6d4f184d0d0e3202
borisachen <boris.chen@gmail.com>
2017-04-25 22:09:02 -0700

    apple

--- 114. Flatten Binary Tree to Linked List.py
+++ 114. Flatten Binary Tree to Linked List.py
@@ -28,6 +28,18 @@ stop when we get to the bottom left, call in A
 continue in order traversal, and continue to append to the left
 once we are done, switch everything to the right
 
+better recursive:
+create a pointer to left and right
+set node.left to null
+flatten the copy of left
+flatten the copy of right
+set node.right = flattened left, because the left side should go first
+then we need to find the end of the path we just appended.
+set cur = node, then iterate to the right child until we hit the end
+once we find the end, set the right child to the right copy
+
+
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
@@ -36,26 +48,58 @@ once we are done, switch everything to the right
 #         self.right = None
 
 class Solution(object):
-	def flatten(self, root):
-		"""
-		:type root: TreeNode
-		:rtype: void Do not return anything, modify root in-place instead.
-		"""
+  def flatten(self, root):
+    """
+    :type root: TreeNode
+    :rtype: void Do not return anything, modify root in-place instead.
+    """
+    if not root: return None
+    left = root.left
+    right = root.right
+    node.left = None
+    self.flatten(left)
+    self.flatten(right)
+    node.right = left
+    temp = node
+    while temp.right: temp = temp.right
+    temp.right = right
 
 
 
 
-post order traversal:
 
 
-private TreeNode prev = null;
 
-public void flatten(TreeNode root) {
-    if (root == null)
-        return;
-    flatten(root.right);
-    flatten(root.left);
-    root.right = prev;
-    root.left = null;
-    prev = root;
-}
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution(object):
+  def flatten(self, root):
+    """
+    :type root: TreeNode
+    :rtype: void Do not return anything, modify root in-place instead.
+    """
+    if not root: return None
+    left = root.left
+    right = root.right
+    root.left = None
+    self.flatten(left)
+    self.flatten(right)
+    root.right = left
+    cur = root
+    while cur.right:
+      cur = cur.right
+    cur.right = right
+
+
+
+
