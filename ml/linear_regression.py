04d4f9142d340997123fb1e8641f977ad3c5bf3d
Boris Chen <boris.chen@gmail.com>
2017-05-15 16:55:48 -0700

    mon

--- ml/linear_regression.py
+++ ml/linear_regression.py
@@ -3,7 +3,25 @@ import numpy as np
 from sklearn.datasets import load_iris
 
 class LinearRegression():
-	pass
+	
+	def __init__(self, n_iter=100, learning_rate=0.01, gradient_descent=True):
+		self.w = None
+		self.n_iter = n_iter
+		self.learning_rate = learning_rate
+		self.gradient_descent = gradient_descent
+		
+	def fit(self, X, y):
+		X = np.insert(X, 0, 1, axis=1)
+		if self.gradient_descent:
+			n_features = np.shape(X)[1]
+			self.w = np.random.random((n_features,))
+			for _ in range(self.n_iter):
+				w_gradient = X.T.dot(X.dot(self.w)-y)
+				self.w -= self.learning_rate*w_gradient
+				
+	def predict(self, X):
+		X = np.insert(X, 0, 1, axis=1)
+		y_pred = X.dot(self.w)
 
 def loadData():
 	iris = load_iris()
@@ -11,11 +29,10 @@ def loadData():
 
 #def main():
 data = loadData()
-np.random.choice()
-x_train
-x_test
-y_train
-y_test
+clr = LinearRegression()
+X = data[:,:3]
+y = data[:,3:]
+clr.fit(X,y)
 
 if __name__ == "__main__":
 	main()
