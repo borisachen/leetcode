e50f601ce4ed5fafbb5f35e2c886533c90d39dfd
Boris Chen <boris.chen@gmail.com>
2017-05-22 17:19:15 -0700

    mon

--- 209. Minimum Size Subarray Sum.py
+++ 209. Minimum Size Subarray Sum.py
@@ -1,20 +1,27 @@
 209. Minimum Size Subarray Sum
 
-DescriptionHintsSubmissionsSolutions
-Total Accepted: 78396
-Total Submissions: 262508
-Difficulty: Medium
-Contributor: LeetCode
-Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
-
 For example, given the array [2,3,1,2,4,3] and s = 7,
 the subarray [4,3] has the minimal length under the problem constraint.
 
+1. two pointers
+right move one at a time
+each step, try to move left up as mmuch as possible while keeping current sum under s
+	check if current length is better than current leader
+
 class Solution(object):
-    def minSubArrayLen(self, s, nums):
-        """
-        :type s: int
-        :type nums: List[int]
-        :rtype: int
-        """
-        
+	def minSubArrayLen(self, s, nums):
+		"""
+		:type s: int
+		:type nums: List[int]
+		:rtype: int
+		"""
+		left = 0
+		curr_sum = 0
+		res = len(nums)+1
+		for right in range(len(nums)):
+			curr_sum += nums[right]
+			while curr_sum >= s:
+				res = min(res, right-left+1)
+				curr_sum -= nums[left]
+				left += 1
+		return result if result <= len(nums) else 0
--- 210. Course Schedule II.py
+++ 210. Course Schedule II.py
@@ -1,14 +1,12 @@
 210. Course Schedule II 
-DescriptionHintsSubmissionsSolutions
-Total Accepted: 58386
-Total Submissions: 216391
-Difficulty: Medium
-Contributor: LeetCode
+
 There are a total of n courses you have to take, labeled from 0 to n - 1.
 
-Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, 
+which is expressed as a pair: [0,1]
 
-Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+Given the total number of courses and a list of prerequisite pairs, 
+return the ordering of courses you should take to finish all courses.
 
 There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
 
@@ -25,10 +23,25 @@ The input prerequisites is a graph represented by a list of edges, not adjacency
 You may assume that there are no duplicate edges in the input prerequisites.
 
 class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
-        """
-        :type numCourses: int
-        :type prerequisites: List[List[int]]
-        :rtype: List[int]
-        """
-        
+	def findOrder(self, numCourses, prerequisites):
+		"""
+		:type numCourses: int
+		:type prerequisites: List[List[int]]
+		:rtype: List[int]
+		"""
+		dic = collections.defaultdict(set)
+		neigh = collections.defaultdict(set)
+		for i, j in prerequisites:
+			dic[i].add(j)
+			neigh[j].add(i)
+		stack = [i for i in range(numCourses) if not dic[i]]
+		res = []
+		while stack:
+			node = stack.pop()
+			res.append(node)
+			for i in neigh[node]:
+				dic[i].remove(node)
+				if not dic[i]:
+					stack.append(i)
+			dic.pop(node)
+		return res if not dic else []
