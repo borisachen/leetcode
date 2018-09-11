79fde1c4a76e8b1bb2db2d5a08153002c9208fa4
borisachen <boris.chen@gmail.com>
2018-09-10 21:03:32 -0700

    mon pm

--- 311. Sparse Matrix Multiplication.py
+++ 311. Sparse Matrix Multiplication.py
@@ -31,6 +31,9 @@ this is a triple for loop, O(N^3)
 Improvements:
 Take advantage of the sparsity.
 When we see a zero, no need to add anything ??? 
+when a[i,k]==0, then no need to add b[k,j]
+  to condition on this, we need to iterate on i,k. so swap the for loop
+Complxity: sparse --> only 2 for loops 
 '''
 
 def multiply(a,b):
@@ -46,6 +49,20 @@ def multiply(a,b):
 				c[i][j] += a[i][k] * b[k][j]
 	return c
 
+
+def multiply_sparse(a,b):
+  nrowa = len(a)
+  ncola = len(a[0])
+  nrowb = len(b)
+  ncolb = len(b[0])
+  # assertion, ncola==nrowb
+  c = [[0] * ncolb for x in range(nrowa)]
+  for i in range(nrowa):
+    for k in range(ncola):
+      if a[i][k] != 0:
+        for j in range(ncolb):
+          c[i][j] += a[i][k] * b[k][j]
+  return c
 
 A = [
   [ 1, 0, 0],
