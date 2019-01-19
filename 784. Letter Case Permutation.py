c8232697eabacbb921beab7061af0e8caceae72f
Boris Chen <boris.chen@gmail.com>
2019-01-18 22:41:17 -0800

    fri

--- 784. Letter Case Permutation.py
+++ 784. Letter Case Permutation.py
@@ -1,7 +1,7 @@
 784. Letter Case Permutation
+Easy/486/61
 
-Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.  
-Return a list of all possible strings we could create.
+Given a string S, we can transform every letter individually to be lowercase or uppercase to create another string.  Return a list of all possible strings we could create.
 
 Examples:
 Input: S = "a1b2"
@@ -14,10 +14,9 @@ Input: S = "12345"
 Output: ["12345"]
 Note:
 
-S will be a string with length at most 12.
+S will be a string with length between 1 and 12.
 S will consist only of letters or digits.
 
----------------------------
 We want to generate all possible sequences, so well do a recursive search.
 Number dont need another branch.
 Alphabet characters require to searches
@@ -47,3 +46,24 @@ letter_case_permutation(S='a1b2')
 letter_case_permutation(S='3z4')
 
 letter_case_permutation(S='12345')
+
+class Solution(object):
+    def letterCasePermutation(self, S):
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        res = []
+        self.back(S, '', 0, res)
+        return res
+
+    def back(self, S, temp, i, res):
+        if len(temp) == len(S):
+            res.append(temp)
+            return
+        if S[i].isdigit():
+            self.back(S, temp+S[i], i+1, res)
+        else:
+            self.back(S, temp+S[i].lower(), i+1, res)
+            self.back(S, temp+S[i].upper(), i+1, res)
+        return
