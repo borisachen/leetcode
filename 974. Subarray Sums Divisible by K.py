d26006250f6cd211b1b1911b972756ecf7ad5386
borisachen <boris.chen@gmail.com>
2019-02-04 16:13:36 -0800

    mon

--- 974. Subarray Sums Divisible by K.py
+++ 974. Subarray Sums Divisible by K.py
@@ -1,7 +1,8 @@
 974. Subarray Sums Divisible by K
 Medium/126/7
 
-Given an array A of integers, return the number of (contiguous, non-empty) subarrays that have a sum divisible by K.
+Given an array A of integers, return the number of (contiguous, non-empty)
+subarrays that have a sum divisible by K.
 
 Example 1:
 
@@ -16,7 +17,20 @@ Note:
 1 <= A.length <= 30000
 -10000 <= A[i] <= 10000
 2 <= K <= 10000
+-----
+The naive solution is to check all possible subarrays. There are O(n^2) of them.
+Sorting doesn't help here since subarrays require order to be preserved,
+which means we are looking for an O(n) algorithm.
 
+Can we preprocess anything?
+presum[i] = sum( A[0] + ... + A[i])
+
+Then sum(i,j) = presum[j] - presum[i]
+Is there something we can store in a hashmap as we preprocess?
+presum = [4, 9, 9,  7,  4, 5]
+A =      [4, 5, 0, -2, -3, 1]
+remainder(1, 1, 1, 3, 2, 0)
+-----
 class Solution(object):
     def subarraysDivByK(self, A, K):
         """
@@ -24,3 +38,11 @@ class Solution(object):
         :type K: int
         :rtype: int
         """
+        res = 0
+        prefix = 0
+        count = [1] + [0] * K
+        for a in A:
+            prefix = (prefix + a) % K
+            res += count[prefix]
+            count[prefix] += 1
+        return res
