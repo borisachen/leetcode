c2dd9a4f2dbceb2709fd1e852f4c8f8f7b694141
borisachen <boris.chen@gmail.com>
2019-02-11 16:28:15 -0800

    mon

---
+++ 975. Odd Even Jump.py
@@ -0,0 +1,109 @@
+975. Odd Even Jump
+Hard
+
+109
+
+19
+
+Favorite
+
+Share
+You are given an integer array A.  From some starting index, you can make a series of jumps.  The (1st, 3rd, 5th, ...) jumps in the series are called odd numbered jumps, and the (2nd, 4th, 6th, ...) jumps in the series are called even numbered jumps.
+
+You may from index i jump forward to index j (with i < j) in the following way:
+
+During odd numbered jumps (ie. jumps 1, 3, 5, ...), you jump to the index j such that A[i] <= A[j] and A[j] is the smallest possible value.  If there are multiple such indexes j, you can only jump to the smallest such index j.
+During even numbered jumps (ie. jumps 2, 4, 6, ...), you jump to the index j such that A[i] >= A[j] and A[j] is the largest possible value.  If there are multiple such indexes j, you can only jump to the smallest such index j.
+(It may be the case that for some index i, there are no legal jumps.)
+A starting index is good if, starting from that index, you can reach the end of the array (index A.length - 1) by jumping some number of times (possibly 0 or more than once.)
+
+Return the number of good starting indexes.
+
+
+
+Example 1:
+
+Input: [10,13,12,14,15]
+Output: 2
+Explanation:
+From starting index i = 0, we can jump to i = 2 (since A[2] is the smallest among A[1], A[2], A[3], A[4] that is greater or equal to A[0]), then we can't jump any more.
+From starting index i = 1 and i = 2, we can jump to i = 3, then we can't jump any more.
+From starting index i = 3, we can jump to i = 4, so we've reached the end.
+From starting index i = 4, we've reached the end already.
+In total, there are 2 different starting indexes (i = 3, i = 4) where we can reach the end with some number of jumps.
+Example 2:
+
+Input: [2,3,1,1,4]
+Output: 3
+Explanation:
+From starting index i = 0, we make jumps to i = 1, i = 2, i = 3:
+
+During our 1st jump (odd numbered), we first jump to i = 1 because A[1] is the smallest value in (A[1], A[2], A[3], A[4]) that is greater than or equal to A[0].
+
+During our 2nd jump (even numbered), we jump from i = 1 to i = 2 because A[2] is the largest value in (A[2], A[3], A[4]) that is less than or equal to A[1].  A[3] is also the largest value, but 2 is a smaller index, so we can only jump to i = 2 and not i = 3.
+
+During our 3rd jump (odd numbered), we jump from i = 2 to i = 3 because A[3] is the smallest value in (A[3], A[4]) that is greater than or equal to A[2].
+
+We can't jump from i = 3 to i = 4, so the starting index i = 0 is not good.
+
+In a similar manner, we can deduce that:
+From starting index i = 1, we jump to i = 4, so we reach the end.
+From starting index i = 2, we jump to i = 3, and then we can't jump anymore.
+From starting index i = 3, we jump to i = 4, so we reach the end.
+From starting index i = 4, we are already at the end.
+In total, there are 3 different starting indexes (i = 1, i = 3, i = 4) where we can reach the end with some number of jumps.
+Example 3:
+
+Input: [5,1,3,4,2]
+Output: 3
+Explanation:
+We can reach the end from starting indexes 1, 2, and 4.
+
+
+'''
+Here is my understanding. Take next_higher as an example, which represents the
+movement at odd numbered jumps from each position in A.
+
+Firstly, note that the list of [a, i] will be sorted according to both a and i.
+Specifically, the order will be first determined by a. Among those with equal a,
+the tie is broken by comparing i. For example, assume that A = [2, 3, 2, 2],
+then the sorted list will be [[2, 0], [2, 2], [2, 3], [3, 1]]. In addition, we
+initially have next_higher = [0, 0, 0, 0].
+
+Given this setting, it can be seen that stack stores the indices of those slots
+in next_higher which need to be assigned a value. That is, those indices are of
+positions from which the odd numbered jump remains uncertain. After looping
+through the first element of the sorted list (i.e., [2, 0]), we have stack = [0].
+When it comes to the second element [2, 2], the descriptions of odd numbered
+jumps are satisfied.
+
+Indices 0 < 2. In other words, this is a jump forward.
+2 is the smallest value in A such that >= 2, as a result of sorting.
+The index 2 is guaranteed to be the smallest index meeting the description 2.
+Recall how we break the tie among those [a, i] with equal a.
+Therefore, it can be inferred that next_higher[0] = 2.
+'''
+
+class Solution(object):
+    def oddEvenJumps(self, A):
+        n = len(A)
+        next_higher, next_lower = [0] * n, [0] * n
+
+        stack = []
+        for a, i in sorted([a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_higher[stack.pop()] = i
+            stack.append(i)
+
+        stack = []
+        for a, i in sorted([-a, i] for i, a in enumerate(A)):
+            while stack and stack[-1] < i:
+                next_lower[stack.pop()] = i
+            stack.append(i)
+
+        higher, lower = [0] * n, [0] * n
+        higher[-1] = lower[-1] = 1
+        for i in range(n - 1)[::-1]:
+            higher[i] = lower[next_higher[i]]
+            lower[i] = higher[next_lower[i]]
+        return sum(higher)
