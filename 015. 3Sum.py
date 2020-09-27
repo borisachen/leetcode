119dbd175bf6e5bf244ea164d70ffcd3c64f892c
borisachen <boris.chen@gmail.com>
2020-09-26 18:54:22 -0700

    sun

--- 015. 3Sum.py
+++ 015. 3Sum.py
@@ -52,3 +52,40 @@ class Solution(object):
     	return ans
 
 Solution().threeSum([-1, 0, 1, 2, -1, -4])
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        n = len(nums)
+        res = []
+        for i in range(n-2):
+            if i > 0 and nums[i] == nums[i+1]:
+                continue
+            l = i + 1
+            l = n - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1 
+                    l += 1
+                    r += 1
+        return ans
+
+
+
+
+
+
+
